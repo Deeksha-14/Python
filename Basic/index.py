99e9c0731110c4528561a77516f279c84697eee6
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-09 15:39:31 +0530

    Create index.py

---
+++ Basic/index.py
@@ -0,0 +1,17 @@
+course = 'python'
+print(course)
+print(course[5])
+print(course[4])
+print(course[3])
+print(course[2])
+print(course[1])
+print(course[0]) #p
+print('---')
+print(course[-0]) #p
+print(course[-1]) #y
+print(course[-2])
+print(course[-3])
+print(course[-4])
+print(course[-5])
+print('---')
+#print(course[6]) #IndexError: string index out of range

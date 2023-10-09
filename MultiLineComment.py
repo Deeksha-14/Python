bc7c228da37f1f1bb8de00d7f87dd0f7fbce5862
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-09 15:28:47 +0530

    Create MultiLineComment.py

---
+++ MultiLineComment.py
@@ -0,0 +1,14 @@
+course = '''
+Hi readers,
+
+This is a multi line comment in python.
+
+Python is vastly used used now a days
+
+so everyone should have basic knowledge of python
+
+EOF
+
+'''
+
+print(course)

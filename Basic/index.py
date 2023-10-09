aeb1d9ed0e1a5b83817fa0d8277c63c20d7677ee
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-09 15:54:20 +0530

    Update index.py

--- Basic/index.py
+++ Basic/index.py
@@ -14,11 +14,15 @@ print(course[-3])
 print(course[-4])
 print(course[-5])
 print('---')
-#print(course[6]) IndexError: string index out of range
+# print(course[6]) IndexError: string index out of range
 
 
 print(course[0:3])
 print(course[0:])
 print(course[:6])
-print(course[:])
+print(course[:])  # Used to copy or clone
 print(course)
+
+
+another = course[:]
+print('This is another : ', another)

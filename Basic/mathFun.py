7e867bd72a48bb8160b2d23e73eb913587e16a2b
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-10 17:57:00 +0530

    Create mathFun.py

---
+++ Basic/mathFun.py
@@ -0,0 +1,32 @@
+import math
+
+x = 2.9
+print('rounded of 2.9 is', round(x))
+
+
+x1 = 2.6
+print(round(x1))
+
+x2 = 2.5
+print(round(x2))
+
+x3 = 2.4
+print(round(x3))
+
+x4 = -2.6
+print('rounded of -2.6 is ', round(x4))
+print('absolute of -2.6 is ', abs(x4))
+
+# abs always returns a positive number
+
+y = math.sqrt(25)
+print(int(y))
+
+print('ceil of 3.65 is ', math.ceil(3.65))
+print('ceil of 3.05 is ', math.ceil(3.05))
+
+print('floor of 3.65 is ', math.floor(3.65))
+print('floor of 3.05 is ', math.floor(3.05))
+
+print(math.isqrt(25))
+

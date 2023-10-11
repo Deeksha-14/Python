a97691293a45c432bc26148dd328c7168ef5ad6e
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-11 10:12:13 +0530

    Create if.py

---
+++ Basic/if.py
@@ -0,0 +1,19 @@
+is_hot = False
+is_cold = False
+
+# is_hot = False
+# is_cold = True
+
+# is_hot = True
+# is_cold = False
+
+if is_hot:
+    print("this is a hot day")
+    print("Drink cold")
+elif is_cold:
+    print("It's a cold day")
+    print("wear warm clothes")
+else:
+    print("It's a lovely day")
+
+print("Enjoy your day")

6e154bc95ec3fa41df18bf40f9cb0f60813837c6
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-10-11 15:02:44 +0530

    Create compOp.py

---
+++ Basic/compOp.py
@@ -0,0 +1,8 @@
+temp = int(input('Enter current temp in celsius: '))
+
+if temp > 30:
+    print('its a  hot day')
+elif temp < 10:
+    print('its a cold day')
+else:
+    print('its neither hot nor cold')
